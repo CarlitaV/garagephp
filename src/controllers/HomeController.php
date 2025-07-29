c0c26ee1f0b8f32cb81fe1fd8eb90a59d7042c11
CarlitaV <varela_carlita@live.fr>
2025-07-29 11:34:07 +0200

    feat: Ajout de la classe HomeController.php

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\Car;
+
+/**
+ * Gère la logique de la page d'accueil
+ */
+class HomeController extends BaseController{
+
+
+    /**
+     * Affiche la page d'accueil
+     */
+    public function index():void{
+
+        $carModel = new Car();
+
+         // On rend (renvoi) la vue 'home/index' et on lui passe le titre et la liste des voitures.
+        $this->render('home/index',[
+            'title'=>'Accueil - Garage php',
+            'cars'=> $carModel->all()
+        ]);
+    }
+
+}
